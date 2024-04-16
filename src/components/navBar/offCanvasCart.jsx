ea927f76ad4a67cbc4c1f174b4877ee1a2b2aa9c
maurogigena <maurogigena9@gmail.com>
2024-04-15 22:27:12 -0300

    delete line

--- src/components/navBar/offCanvasCart.jsx
+++ src/components/navBar/offCanvasCart.jsx
@@ -1,11 +1,8 @@
-import React, { useContext } from 'react';
 import { Offcanvas } from 'react-bootstrap';
-import { CartContext } from '../../context/cartContext.jsx';
 import './navBar.css';
 import { CartItemList } from '../cartItemList/cartItemList.jsx';
 
-const CartOffcanvas = ({ showCartOffcanvas, handleCartToggle }) => { // eslint-disable-next-line
-  const {} = useContext(CartContext);
+const CartOffcanvas = ({ showCartOffcanvas, handleCartToggle }) => {
   
   return (
     <Offcanvas show={showCartOffcanvas} onHide={handleCartToggle} placement="end">
