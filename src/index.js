2271b02aa7adad3eae2628f608b4bc3ea6f0bb7b
maurogigena <maurogigena9@gmail.com>
2024-04-22 16:34:17 -0300

    FINAL PROYECTO REACT

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Asegúrate de importar Bootstrap si lo estás usando
-import App from './App';
+import App from './App.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
